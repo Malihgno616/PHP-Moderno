3eb573b619d02add2f5d37049b5f11869a7b8c47
Ackin <ackinlino123@gmail.com>
2024-08-16 12:05:50 -0300

    DESAFIO 01 PHP

---
+++ Desafio-01/main.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+
+<body>
+    <section>
+        <h1>Resultado Final</h1>
+        <main>
+            <?php 
+                $num = $_GET["numero"] ?? "Nenhum numero";
+                $ante = $num -1;
+                $suc = $num +1;
+                               
+                print "<p>O numero escolhido foi <strong>$num</strong></p>";
+                echo "<p>O seu antecessor é $ante</p>";
+                echo "<p>O seu sucessor é $suc</p>";       
+                                    
+            ?>
+
+        </main>
+        <button>
+            <a href="index.html">Voltar</a>
+        </button>
+    </section>
+
+
+
+
+
+</body>
+
+</html>
