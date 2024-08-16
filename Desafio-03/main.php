e3746e54d21d40faed2887f773c123fbb8d96655
Ackin.Dev <ackinlino123@gmail.com>
2024-08-16 17:11:52 -0300

    Ajustes

--- Desafio-03/main.php
+++ Desafio-03/main.php
@@ -5,23 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>Conversor de Moedas v1.0</h1>
-    <?php
-        $real = $_GET["numero"];
-        $dolar = $real / 5.47;
-        $dolar = number_format($dolar,2,".","");
+    <section>
+        <h1>Conversor de Moedas v1.0</h1>
+        <?php
+            $real = $_GET["numero"];
+            $dolar = $real / 5.47;
+            $dolar = number_format($dolar,2,".","");
         
-        print "Os seus $real R$ vale $dolar U$";      
-    ?>
-    <form action="index.html">
+            print "Os seus $real R$ vale $dolar U$";
+        ?>
+        <form action="index.html">
 
-        <a href="index.html">
-            <input type="submit" value="Voltar">
-        </a>
-    </form>
+            <a href="index.html">
+                <input type="submit" value="Voltar">
+            </a>
+        </form>
+    </section>
+
 </body>
 
 </html>
